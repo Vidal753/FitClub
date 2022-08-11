0c430b4b6b3991adaadc5340c8268d2348f18c36
vidalbaquedano <vidalbaquedano@gmail.com>
2022-08-11 17:02:03 -0600

    hero

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import './Hero.css';
+import Header from "../Header/Header";
+
+const Hero = () =>{
+    return(
+        <div className={'hero'}>
+            <div className={'left-h'}>
+                <Header/> 
+                <div className="the-best-ad">
+                    <div></div>
+                    <span>The best fitness club in the town</span>
+                </div>
+                <div className="hero-text">
+                     <div>
+                        <span>Shape </span>
+                        <span>Your</span>
+                    </div>
+                </div>
+            </div>
+            <div className={'right-h'}>right side</div>
+        </div>
+    )
+}
+
+export default Hero;
