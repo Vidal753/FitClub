08e16725a6b0b12de5eb723400f1bea56fab5fc5
vidalbaquedano <vidalbaquedano@gmail.com>
2022-08-14 14:31:07 -0600

    hero-completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-import Hero from "./components/Hero/Hero";
+import Hero from './components/Hero/Hero';
+
 function App() {
   return (
     <div className="App">
-          <Hero/>
+      <Hero />
     </div>
   );
 }
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import './Hero.css';
 import Header from '../Header/Header';
+import hero_image from '../../assets/hero_image.png';
+import hero_image_back from '../../assets/hero_image_back.png';
+import Calories from '../../assets/calories.png';
+import Heart from '../../assets/heart.png';
 
 function Hero() {
   return (
@@ -13,15 +17,62 @@ function Hero() {
         </div>
         <div className="hero-text">
           <div>
-            <span>Shape </span>
+            <span className="stroke-text">Shape </span>
             <span>Your</span>
           </div>
           <div>
             <span>Idea body</span>
           </div>
+          <div>
+            {/* eslint-disable-next-line max-len */}
+            <span>Ideal body in here we will help you to shape and build your ideal body and live up your life to fullest</span>
+          </div>
+        </div>
+
+        {/* figures */}
+
+        <div className="figures">
+          <div>
+            <span>+140</span>
+            <span>expert coachs</span>
+          </div>
+          <div>
+            <span>+978</span>
+            <span>members joined</span>
+          </div>
+          <div>
+            <span>+50</span>
+            <span>fitness programs</span>
+          </div>
+        </div>
+
+        {/* hero buttons */}
+        <div className="hero-buttons">
+          <button className="btn">Get Started</button>
+          <button className="btn">Learn more</button>
         </div>
       </div>
-      <div className="right-h">right side</div>
+      <div className="right-h">
+        <button className="btn">Join Now</button>
+        <div className="heart-rate">
+          <img src={Heart} alt="" />
+          <span>Heart rate</span>
+          <span>116 bpm</span>
+        </div>
+
+        {/* hero images */}
+        <img className="hero-image" src={hero_image} alt="" />
+        <img className="hero-image-back" src={hero_image_back} alt="" />
+
+        {/* calories */}
+        <div className="calories">
+          <img src={Calories} alt="" />
+          <div>
+            <span>Calories Burned</span>
+            <span>220 kcl</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
